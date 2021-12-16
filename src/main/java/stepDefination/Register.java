0e77258436e886949576641f65b8d631c842b0b6
Pratiksha <pratikshagund@gmail.com>
2021-12-16 16:56:04 +0530

    Register Page added1

---
+++ src/main/java/stepDefination/Register.java
@@ -0,0 +1,13 @@
+package stepDefination;
+
+public class Register {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("Register page");
+	}
+
+}
